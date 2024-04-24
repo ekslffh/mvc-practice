6a17b27ec01b22ceba05e9db8de9cbcaeaff81ad
ekslffh <ekslffh@gmail.com>
2024-04-24 17:45:14 +0900

    MVC 프레임워크 직접 만들어보기

---
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,62 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.ModelAndView;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    private RequestMappingHandlerMapping rmhm;
+
+    private List<HandlerAdapter> handlerAdapters;
+
+    private List<ViewResolver> viewResolvers;
+
+    @Override
+    public void init() throws ServletException {
+        rmhm = new RequestMappingHandlerMapping();
+        rmhm.init();
+
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter());
+        viewResolvers = Collections.singletonList(new JspViewResolver());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("[DispatcherServlet] service started");
+        try {
+            Controller handler = rmhm.findHandler(new HandlerKey(RequestMethod.valueOf(request.getMethod()), request.getRequestURI()));
+
+            HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                    .filter(ha -> ha.supports(handler))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No adapter for handler [" + handler + "]"));
+
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(modelAndView.getViewName());
+                view.render(modelAndView.getModel(), request, response);
+            }
+        } catch (Exception e) {
+            log.error("exception occurred : [{}]", e.getMessage(), e);
+            throw new ServletException(e);
+        }
+    }
+}
